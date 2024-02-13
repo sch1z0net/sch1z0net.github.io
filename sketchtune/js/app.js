f45599fc3768383620a44a3f10ee373d5d29fc09
sch1z0net <eindudevommond@gmail.com>
2024-02-13 19:18:24 +0100

    scrolling test

--- sketchtune/js/app.js
+++ sketchtune/js/app.js
@@ -7,15 +7,9 @@ $(document).ready(function() {
       this.name = name;
 
      $(this).bind('wheel', function(e) {
-      if(e.originalEvent.deltaY / 120 > 0) {
-         //alert('up');
-         $("track-window").scrollTop(e.originalEvent.deltaY);
-      } else {
-         //alert('down');
-         $("track-window").scrollTop(e.originalEvent.deltaY);
-      }
+       $("track-window").scrollTop(e.originalEvent.deltaY + $("track-window").scrollTop());
+       $("beat-bar").scrollLeft(e.originalEvent.deltaX + $("beat-bar").scrollLeft());
      });
-
     }
 
     connectedCallback() {
