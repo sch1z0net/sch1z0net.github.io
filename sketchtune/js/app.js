e17f1a9ae58f4d7c68abbf9435503fb388abb4e8
sch1z0net <eindudevommond@gmail.com>
2024-02-13 14:12:16 +0100

    different columns

--- sketchtune/js/app.js
+++ sketchtune/js/app.js
@@ -1,51 +1,61 @@
 
-class GridWindow extends HTMLElement {
-  constructor(name) {
-    super();
-    this.name = name;
-  }
+$(document).ready(function() {
 
-  connectedCallback() {
+  class GridWindow extends HTMLElement {
+    constructor(name) {
+      super();
+      this.name = name;
+    }
+
+    connectedCallback() {
         // Add any initial setup logic here
-  }
-}
-
-class BeatBar extends HTMLElement {
-  constructor(name) {
-    super();
-    this.name = name;
+    }
   }
 
-  connectedCallback() {
+  class BeatBar extends HTMLElement {
+    constructor(name) {
+      super();
+      this.name = name;
+    }
+
+    connectedCallback() {
         // Add any initial setup logic here
-  }
-}
-
-class BeatCol extends HTMLElement {
-  constructor(name) {
-    super();
-    this.name = name;
+    }
   }
 
-  connectedCallback() {
+  class BeatCol extends HTMLElement {
+    constructor(name) {
+      super();
+      this.name = name;
+      var beat = $(this).attr('data-beat');
+      if(beat % 8 < 4){
+         $(this).addClass("col_b");
+      }else{
+         $(this).addClass("col_a");
+      }
+      
+    }
+
+    connectedCallback() {
+
+    }
+  }
+
+  class Track extends HTMLElement {
+    constructor(name) {
+      super();
+      this.name = name;
+    }
+
+    connectedCallback() {
         // Add any initial setup logic here
-  }
-}
-
-class Track extends HTMLElement {
-  constructor(name) {
-    super();
-    this.name = name;
+    }
   }
 
-  connectedCallback() {
-        // Add any initial setup logic here
-  }
-}
+  // Define the custom element
+  customElements.define('grid-window', GridWindow);
+  customElements.define('beat-bar', BeatBar);
+  customElements.define('beat-col', BeatCol);
 
 
-
-// Define the custom element
-customElements.define('grid-window', GridWindow);
-customElements.define('beat-bar', BeatBar);
-customElements.define('beat-col', BeatCol);
+});
