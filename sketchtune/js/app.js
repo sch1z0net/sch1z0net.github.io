d8c066d169f61a066e4ef88e90a431df7c93186b
sch1z0net <eindudevommond@gmail.com>
2024-02-13 13:35:18 +0100

    grid work

--- sketchtune/js/app.js
+++ sketchtune/js/app.js
@@ -10,6 +10,27 @@ class GridWindow extends HTMLElement {
   }
 }
 
+class BeatBar extends HTMLElement {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+
+  connectedCallback() {
+        // Add any initial setup logic here
+  }
+}
+
+class BeatCol extends HTMLElement {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+
+  connectedCallback() {
+        // Add any initial setup logic here
+  }
+}
 
 class Track extends HTMLElement {
   constructor(name) {
@@ -22,5 +43,9 @@ class Track extends HTMLElement {
   }
 }
 
+
+
 // Define the custom element
 customElements.define('grid-window', GridWindow);
+customElements.define('beat-bar', BeatBar);
+customElements.define('beat-col', BeatCol);
