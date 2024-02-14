c16d236292c971773921767d2b572fda470d211b
sch1z0net <eindudevommond@gmail.com>
2024-02-14 14:48:06 +0100

    Update app.js

--- sketchtune/js/app.js
+++ sketchtune/js/app.js
@@ -132,7 +132,7 @@
 
         $(this).dblclick(function(event) {
            const x = event.clientX + $("beat-bar").scrollLeft();
-           var newmargin = (Math.round(x / 20) * 20) + "px";
+           var newmargin = (Math.round(x / 20) * 20);
            var newpat = $('<track-pattern>').css("margin-left",newmargin+"px");
            $(this).append(newpat);
         });
