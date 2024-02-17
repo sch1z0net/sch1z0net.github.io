1026eb25a76bc70f981b155c45342b8641bb33f8
sch1z0net <eindudevommond@gmail.com>
2024-02-17 13:36:59 +0100

    Update app.js

--- sketchtune/js/app.js
+++ sketchtune/js/app.js
@@ -1014,8 +1014,7 @@ $(document).ready(function(){
 
   button_load.on("click", function(){
       context = new AudioContext();
-      setupSamples(context).then((_samples) => {
-        samples = _samples;
+      setupSamples(context).then(() => {
         button_play.css("display","inline-block");
         button_play.on("click", function(){ 
           is_playing = false;
