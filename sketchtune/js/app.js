c682d3783a7333d655b256b180f882290fa48c53
sch1z0net <eindudevommond@gmail.com>
2024-02-18 14:01:25 +0100

    Update app.js

--- sketchtune/js/app.js
+++ sketchtune/js/app.js
@@ -53,8 +53,7 @@
     WPB = BEAT_WIDTH;
     $("#bpm").val(BPM);
     resizeTimeBar();
-    console.log("Update BPM to: ",BPM);
-    console.log("at Beat: ",(Math.floor(time_marker_in_beats/4)+1)+"."+(Math.floor(time_marker_in_beats%4)+1));
+    console.log("⚙︎",BPM,"at",(Math.floor(time_marker_in_beats/4)+1)+"."+(Math.floor(time_marker_in_beats%4)+1));
     resetClock();
     setStartOffsetInBeats(time_marker_in_beats); 
     updateTimeMarker();
