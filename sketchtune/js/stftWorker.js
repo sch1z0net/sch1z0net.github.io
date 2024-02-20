68173e383215b819d2e181eba0fd692b78ce05c6
sch1z0net <eindudevommond@gmail.com>
2024-02-20 22:40:54 +0100

    Update stftWorker.js

--- sketchtune/js/stftWorker.js
+++ sketchtune/js/stftWorker.js
@@ -2,22 +2,38 @@ importScripts('./fft.js');
 
 // Function to perform Short-Time Fourier Transform (STFT)
 function STFT(inputSignalChunk, windowSize, hopSize) {
-    const spectrogramChunk = [];
+    return new Promise((resolve, reject) => {
+        const spectrogramChunk = [];
 
-    // Process each frame in the chunk
-    for (let i = 0; i <= inputSignalChunk.length - windowSize; i += hopSize) {
-        const frame = inputSignalChunk.slice(i, i + windowSize);
-        const windowedFrame = applyHanningWindow(frame);
-        const spectrum = computeFFT(windowedFrame);
-        spectrogramChunk.push(spectrum);
-    }
+        // Process each frame in the chunk asynchronously
+        const processFrames = async () => {
+            try {
+                for (let i = 0; i <= inputSignalChunk.length - windowSize; i += hopSize) {
+                    const frame = inputSignalChunk.slice(i, i + windowSize);
+                    const windowedFrame = applyHanningWindow(frame);
+                    const spectrum = await computeFFTAsync(windowedFrame); // Assuming computeFFT has an asynchronous version
+                    spectrogramChunk.push(spectrum);
+                }
+                resolve(spectrogramChunk);
+            } catch (error) {
+                reject(error);
+            }
+        };
 
-    // Send the result back to the main thread
-    postMessage(spectrogramChunk);
+        processFrames();
+    });
 }
 
 // Listen for messages from the main thread
 onmessage = function (e) {
     const { inputSignal, windowSize, hopSize } = e.data;
-    STFT(inputSignal, windowSize, hopSize);
+    STFT(inputSignal, windowSize, hopSize)
+        .then((spectrogramChunk) => {
+            // Send the result back to the main thread
+            postMessage(spectrogramChunk);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            // Optionally, handle the error and send back an error message to the main thread
+        });
 };
