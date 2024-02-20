0c0a798d42725e819e310146ffc11b1a203ab696
sch1z0net <eindudevommond@gmail.com>
2024-02-21 00:23:04 +0100

    Update stretcher.js

--- sketchtune/js/stretcher.js
+++ sketchtune/js/stretcher.js
@@ -74,7 +74,8 @@ function STFTWithWebWorkers(inputSignal, windowSize, hopSize) {
 
         // Create worker and send the chunk of inputSignal
         const worker = new Worker('./js/stftWorker.js');
-        worker.postMessage({ inputSignal: chunk, windowSize, hopSize, fftFactorLookup });
+        //worker.postMessage({ inputSignal: chunk, windowSize, hopSize, fftFactorLookup });
+        worker.postMessage({ bla: "hello"});
 
         // Listen for messages from the worker
         worker.onmessage = function (e) {
