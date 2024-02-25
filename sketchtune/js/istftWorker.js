d27f9422a3e4fa992333ab1c84d95c15deb3f535
sch1z0net <eindudevommond@gmail.com>
2024-02-25 22:20:43 +0100

    Update istftWorker.js

--- sketchtune/js/istftWorker.js
+++ sketchtune/js/istftWorker.js
@@ -85,7 +85,9 @@ function ISTFT_OLA_NORMALIZED(spectrogramChunk, windowSize, hopSize, workerID) {
 function ISTFT_WOLA(spectrogramChunk, windowSize, hopSize, workerID, windowType, halfSpec) {
     return new Promise((resolve, reject) => {
         let spectra = spectrogramChunk.length;
-        const outputSignalChunk = new Float32Array(spectra * hopSize);
+        let outputLength = spectra * hopSize;
+        if(halfSpec){ outputLength /= 2; }
+        const outputSignalChunk = new Float32Array(outputLength);
         
         // Process each frame in the spectrogram chunk asynchronously
         const processFrames = async () => {
