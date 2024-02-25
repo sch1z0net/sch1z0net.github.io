9729aa8b4057cf9e47c3f6c2975b7e43fdc7c561
sch1z0net <eindudevommond@gmail.com>
2024-02-25 21:44:26 +0100

    test

--- sketchtune/js/istftWorker.js
+++ sketchtune/js/istftWorker.js
@@ -155,7 +155,7 @@ onmessage = function (e) {
         reconstructedChunk.push(frame);
     }
 
-    console.log("CHUNK2",reconstructedChunk);
+    console.log("CHUNK2",workerID,reconstructedChunk);
     
     //ISTFT_OLA(reconstructedChunk, windowSize, hopSize, workerID, halfSpec)
     //ISTFT_OLA_NORMALIZED(reconstructedChunk, windowSize, hopSize, workerID, halfSpec)
