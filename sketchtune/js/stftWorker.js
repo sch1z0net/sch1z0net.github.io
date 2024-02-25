ea413939c69ba13e0ef5b252be667a96438c3c2c
sch1z0net <eindudevommond@gmail.com>
2024-02-25 12:16:48 +0100

    Update stftWorker.js

--- sketchtune/js/stftWorker.js
+++ sketchtune/js/stftWorker.js
@@ -193,7 +193,7 @@ onmessage = function (e) {
 
     STFT(chunk, windowSize, hopSize, numFrames, mode)
         .then((spectrogramChunk) => {
-            const flattenedFrames = spectrogramChunk.map(frame => frame.flatMap(spectrum => [spectrum.re, spectrum.im]) );
+            const flattenedChunk = spectrogramChunk.map(frame => frame.flatMap(spectrum => [spectrum.re, spectrum.im]) );
             console.log(flattenedChunk);
 
             // Convert the flattened array to a Float32Array
