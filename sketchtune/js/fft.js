6d5a3431a44d42dae2cdbacec62963327579478d
sch1z0net <eindudevommond@gmail.com>
2024-02-26 10:06:17 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -628,8 +628,8 @@ async function computeInverseFFT(spectrum) {
     }
 
     for (let i = 0; i < paddedSpectrum.length/2; i++){
-        console.log(paddedSpectrum[i * 2], paddedSpectrum[(paddedSize-1) * 2 - i * 2]);
-        console.log(paddedSpectrum[i * 2 + 1], paddedSpectrum[(paddedSize-1) * 2 - i * 2 + 1]);
+        console.log(paddedSpectrum[i * 2], paddedSpectrum[paddedSize * 2 - (i+1) * 2]);
+        console.log(paddedSpectrum[i * 2 + 1], paddedSpectrum[paddedSize * 2 - (i+1) * 2 + 1]);
         /*if(paddedSpectrum[i * 2] != paddedSpectrum[(paddedSize-1) * 2 - i * 2]){
             throw(Error("Spectrum not symmetric at index: "+i));
         }*/
