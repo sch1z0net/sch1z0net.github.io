120bc2e19b36621e856767216e404fa39f880365
sch1z0net <eindudevommond@gmail.com>
2024-02-26 23:50:31 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -1027,7 +1027,7 @@ async function computeInverseFFTonHalf(halfSpectrum) {
 
 /****** TESTING PERFORMANCE ******/
 
-fftRealInPlaceRADIX2([1,2,3,4,5,6,7,8]);
+console.log(fftRealInPlaceRADIX2([1,2,3,4,5,6,7,8]));
 
 
 
