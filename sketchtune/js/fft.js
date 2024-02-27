5138feb6f1b34883e2ab53a5c2a46c6d6623bcd4
sch1z0net <eindudevommond@gmail.com>
2024-02-27 23:36:38 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -1242,12 +1242,18 @@ async function computeInverseFFTonHalf(halfSpectrum) {
 //console.log(fftRealInPlaceRADIX2([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]));
 //console.log(fftComplexInPlace([1,0,2,0,3,0,4,0,5,0,6,0,7,0,8,0,9,0,10,0,11,0,12,0,13,0,14,0,15,0,16,0]));
 
+console.log("...................... RADIX 2 [1,2,3,4].............................................");
 console.log(fftRealInPlaceRADIX2([1,2,3,4]));
+console.log("...................... RADIX 2 [1,2,3,4,5,6,7,8].....................................");
 console.log(fftRealInPlaceRADIX2([1,2,3,4,5,6,7,8]));
-
+console.log("...................... RADIX 2 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]..............");
+console.log(fftRealInPlaceRADIX2([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]));
+console.log("...................... RADIX 4 [1,2,3,4].............................................");
 console.log(fftRealInPlaceRADIX4([1,2,3,4]));
+console.log("...................... RADIX 4 [1,2,3,4,5,6,7,8].....................................");
 console.log(fftRealInPlaceRADIX4([1,2,3,4,5,6,7,8]));
-//console.log(fftRealInPlaceRADIX4([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]));
+console.log("...................... RADIX 4 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]..............");
+console.log(fftRealInPlaceRADIX4([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]));
 //console.log(fftRealInPlaceRADIX4([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]));
 
 // Define the number of FFT operations to perform
