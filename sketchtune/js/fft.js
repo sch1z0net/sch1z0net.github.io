d3838469911cf9494bf3d8b66144536f07c04f84
sch1z0net <eindudevommond@gmail.com>
2024-02-27 20:47:06 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -1210,9 +1210,12 @@ async function computeInverseFFTonHalf(halfSpectrum) {
 /****** TESTING PERFORMANCE ******/
 
 //console.log(fftRealInPlaceRADIX2([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]));
-//console.log(fftRealInPlaceRADIX4([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]));
 //console.log(fftComplexInPlace([1,0,2,0,3,0,4,0,5,0,6,0,7,0,8,0,9,0,10,0,11,0,12,0,13,0,14,0,15,0,16,0]));
 
+console.log(fftRealInPlaceRADIX4([1,2,3,4]));
+console.log(fftRealInPlaceRADIX4([1,2,3,4,5,6,7,8,9]));
+console.log(fftRealInPlaceRADIX4([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]));
+console.log(fftRealInPlaceRADIX4([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]));
 
 // Define the number of FFT operations to perform
 const numOperations = 10000; // You can adjust this number based on your requirements
