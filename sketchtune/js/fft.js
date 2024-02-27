0af70b190dc1a3c785ad78074f835ef2bd33b14c
sch1z0net <eindudevommond@gmail.com>
2024-02-27 11:52:16 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -500,15 +500,16 @@ function fftRealInPlaceRADIX4(inputOriginal) {
 
         if (size == N) { inv = -inv; }
 
-        const halfSize = size >> 2;
+        const halfSize = size >> 1;
+        const quarterSize = size >> 2;
         console.log("------------------------ size",size)
         // Loop condition
         while (i < N) {
             // Use precalculated FFT factors directly
-            const tIdxRe1 = pre + (j % halfSize) * 2;
-            const tIdxIm1 = pre + (j % halfSize) * 2 + 1;
-            const tIdxRe2 = pre + ((j % halfSize) + halfSize) * 2;
-            const tIdxIm2 = pre + ((j % halfSize) + halfSize) * 2 + 1;
+            const tIdxRe1 = pre + (j % quarterSize) * 2;
+            const tIdxIm1 = pre + (j % quarterSize) * 2 + 1;
+            const tIdxRe2 = pre + ((j + quarterSize) % quarterSize) * 2;
+            const tIdxIm2 = pre + ((j + quarterSize) % quarterSize) * 2 + 1;
 
             const twiddleRe1 = factors[tIdxRe1];
             const twiddleIm1 = factors[tIdxIm1];
