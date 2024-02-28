c9202c9447541c3dbe93383736a94ce91b641844
sch1z0net <eindudevommond@gmail.com>
2024-02-28 02:53:13 +0100

    test if the problem is stretching

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -1207,7 +1207,7 @@ function IFFT(spectrum) {
 
 
 // Function to compute inverse FFT of a spectrum
-function computeInverseFFT(spectrum) {
+async function computeInverseFFT(spectrum) {
     // Ensure the size of the spectrum array is a power of 2
     const paddedSize = nextPowerOf2(spectrum.length);
 
