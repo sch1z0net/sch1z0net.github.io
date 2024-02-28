28a3faa2115e56977b4fa08a9a99715b554b35d4
sch1z0net <eindudevommond@gmail.com>
2024-02-28 10:41:50 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -130,8 +130,8 @@ function precalculateFFTFactorsRADIX2(maxSampleLength) {
             factors[pre + i * 2] = Math.cos(angle1); // Cosine of angle1
             factors[pre + i * 2 + 1] = Math.sin(angle1); // Sine of angle1
         }
-        pre += N;
         if(maxN==64){ console.log("for N=",N,"LOOKUP goes from",pre,"to",pre+N); }
+        pre += N;
         //2 + 4 + 8 + 16 + 32 + 64...
     }
 
