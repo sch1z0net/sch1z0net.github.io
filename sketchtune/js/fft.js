0b6114571a185930c9b3ec0df193038e1a1e2bee
sch1z0net <eindudevommond@gmail.com>
2024-02-29 21:55:33 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -1501,9 +1501,9 @@ function eff(N){
    for(let p = 1; p<=max_p; p++){
         const accesses_per_it = (2<<(p-1));
         const t_per_it = (2<<(p-1)) * p;
-        const iterations = looplen / (t_per_it/2);  
+        const iterations = looplen * 2 / (t_per_it);  
         const accesses = accesses_per_it * looplen * 2 / (t_per_it);
-        const twiddles = t_per_it * looplen / (t_per_it/2);
+        const twiddles = t_per_it * looplen * 2 / (t_per_it);
         const twiddlelizers = twiddles / 2;
         const ratio =  accesses_per_it / t_per_it;
         console.log("ps ",p,": Iterations ->",iterations.toFixed(0),
