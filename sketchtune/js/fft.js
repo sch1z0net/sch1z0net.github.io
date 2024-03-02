fb525e0ceed1b5270c54b9d0b6d089be6fc19736
sch1z0net <eindudevommond@gmail.com>
2024-03-02 21:43:26 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -899,16 +899,18 @@ function fftComplexInPlace_seq_4(out) {
         let ji = 0;
         console.log( "########################################" );
         for(let i = 0; i < N; i+=4){
+            x0Re = out[(s + k + d*0)*2+0]; x0Im = out[(s + k + d*0)*2+1]; 
+            x1Re = out[(s + k + d*1)*2+0]; x1Im = out[(s + k + d*1)*2+1];
+            x2Re = out[(s + k + d*2)*2+0]; x2Im = out[(s + k + d*2)*2+1];
+            x3Re = out[(s + k + d*3)*2+0]; x3Im = out[(s + k + d*3)*2+1];
+
+
+
             j2 = ji%size1; j1 = ji%(size1/2); ji++;
             tRe_1  = Math.cos((2 * Math.PI * j1) / size1);
             tIm_1  = Math.sin((2 * Math.PI * j1) / size1);
             tRe_2  = Math.cos((2 * Math.PI * j2) / size2);
             tIm_2  = Math.sin((2 * Math.PI * j2) / size2);
-
-            x0Re = out[(s + k + d*0)*2+0]; x0Im = out[(s + k + d*0)*2+1]; 
-            x1Re = out[(s + k + d*1)*2+0]; x1Im = out[(s + k + d*1)*2+1];
-            x2Re = out[(s + k + d*2)*2+0]; x2Im = out[(s + k + d*2)*2+1];
-            x3Re = out[(s + k + d*3)*2+0]; x3Im = out[(s + k + d*3)*2+1];
 
             xM0Re = x0Re + (x1Re * tRe_1 - x1Im * tIm_1);
             xM0Im = x0Im + (x1Re * tIm_1 + x1Im * tRe_1);
@@ -922,30 +924,8 @@ function fftComplexInPlace_seq_4(out) {
 
             out[(0+i)*2+0] = xM0Re + ((xM2Re)*tRe_2 - ((xM2Im)*tIm_2)); // x0re
             out[(0+i)*2+1] = xM0Im + ((xM2Re)*tIm_2 + ((xM2Im)*tRe_2)); // x0im
-            console.log( (s + k + d*0), (s + k + d*1), (s + k + d*2), (s + k + d*3) );
-            console.log( j1, j2 );
-            k += 1; k = k%d;
-
-            j2 = ji%size1; j1 = ji%(size1/2); ji++;
-            tRe_1  = Math.cos((2 * Math.PI * j1) / size1);
-            tIm_1  = Math.sin((2 * Math.PI * j1) / size1);
-            tRe_2  = Math.cos((2 * Math.PI * j2) / size2);
-            tIm_2  = Math.sin((2 * Math.PI * j2) / size2);
-
-            xM0Re = x0Re + (x1Re * tRe_1 - x1Im * tIm_1);
-            xM0Im = x0Im + (x1Re * tIm_1 + x1Im * tRe_1);
-            xM2Re = x2Re + (x3Re * tRe_1 - x3Im * tIm_1);
-            xM2Im = x2Im + (x3Re * tIm_1 + x3Im * tRe_1);
-
-            xM1Re = x0Re - (x1Re * tRe_1 - x1Im * tIm_1);
-            xM1Im = x0Im - (x1Re * tIm_1 + x1Im * tRe_1);
-            xM3Re = x2Re - (x3Re * tRe_1 - x3Im * tIm_1);
-            xM3Im = x2Im - (x3Re * tIm_1 + x3Im * tRe_1);
-
-            out[(1+i)*2+0] = xM1Re - ((xM3Re)*tRe_2 - ((xM3Im)*tIm_2)); // x1re
-            out[(1+i)*2+1] = xM1Im - ((xM3Re)*tIm_2 + ((xM3Im)*tRe_2)); // x1im
             //console.log( (s + k + d*0), (s + k + d*1), (s + k + d*2), (s + k + d*3) );
-            console.log( j1, j2 );
+            //console.log( j1, j2 );
             k += 1; k = k%d;
 
             j2 = ji%size1; j1 = ji%(size1/2); ji++;
@@ -964,35 +944,57 @@ function fftComplexInPlace_seq_4(out) {
             xM3Re = x2Re - (x3Re * tRe_1 - x3Im * tIm_1);
             xM3Im = x2Im - (x3Re * tIm_1 + x3Im * tRe_1);
 
-            out[(2+i)*2+0] = xM0Re + ((xM2Re)*tRe_2 - ((xM2Im)*tIm_2)); // x2re
-            out[(2+i)*2+1] = xM0Im + ((xM2Re)*tIm_2 + ((xM2Im)*tRe_2)); // x2im
+            out[(1+i)*2+0] = xM1Re + ((xM3Re)*tRe_2 - ((xM3Im)*tIm_2)); // x1re
+            out[(1+i)*2+1] = xM1Im + ((xM3Re)*tIm_2 + ((xM3Im)*tRe_2)); // x1im
             //console.log( (s + k + d*0), (s + k + d*1), (s + k + d*2), (s + k + d*3) );
-            console.log( j1, j2 );
+            //console.log( j1, j2 );
             k += 1; k = k%d;
-
+            
             j2 = ji%size1; j1 = ji%(size1/2); ji++;
             tRe_1  = Math.cos((2 * Math.PI * j1) / size1);
             tIm_1  = Math.sin((2 * Math.PI * j1) / size1);
             tRe_2  = Math.cos((2 * Math.PI * j2) / size2);
             tIm_2  = Math.sin((2 * Math.PI * j2) / size2);
-
+            
             xM0Re = x0Re + (x1Re * tRe_1 - x1Im * tIm_1);
             xM0Im = x0Im + (x1Re * tIm_1 + x1Im * tRe_1);
             xM2Re = x2Re + (x3Re * tRe_1 - x3Im * tIm_1);
             xM2Im = x2Im + (x3Re * tIm_1 + x3Im * tRe_1);
-
+            
             xM1Re = x0Re - (x1Re * tRe_1 - x1Im * tIm_1);
             xM1Im = x0Im - (x1Re * tIm_1 + x1Im * tRe_1);
             xM3Re = x2Re - (x3Re * tRe_1 - x3Im * tIm_1);
             xM3Im = x2Im - (x3Re * tIm_1 + x3Im * tRe_1);
-
+            
+            out[(2+i)*2+0] = xM0Re - ((xM2Re)*tRe_2 - ((xM2Im)*tIm_2)); // x2re
+            out[(2+i)*2+1] = xM0Im - ((xM2Re)*tIm_2 + ((xM2Im)*tRe_2)); // x2im
+            //console.log( (s + k + d*0), (s + k + d*1), (s + k + d*2), (s + k + d*3) );
+            //console.log( j1, j2 );
+            k += 1; k = k%d;
+            
+            j2 = ji%size1; j1 = ji%(size1/2); ji++;
+            tRe_1  = Math.cos((2 * Math.PI * j1) / size1);
+            tIm_1  = Math.sin((2 * Math.PI * j1) / size1);
+            tRe_2  = Math.cos((2 * Math.PI * j2) / size2);
+            tIm_2  = Math.sin((2 * Math.PI * j2) / size2);
+            
+            xM0Re = x0Re + (x1Re * tRe_1 - x1Im * tIm_1);
+            xM0Im = x0Im + (x1Re * tIm_1 + x1Im * tRe_1);
+            xM2Re = x2Re + (x3Re * tRe_1 - x3Im * tIm_1);
+            xM2Im = x2Im + (x3Re * tIm_1 + x3Im * tRe_1);
+            
+            xM1Re = x0Re - (x1Re * tRe_1 - x1Im * tIm_1);
+            xM1Im = x0Im - (x1Re * tIm_1 + x1Im * tRe_1);
+            xM3Re = x2Re - (x3Re * tRe_1 - x3Im * tIm_1);
+            xM3Im = x2Im - (x3Re * tIm_1 + x3Im * tRe_1);
+            
             out[(3+i)*2+0] = xM1Re - ((xM3Re)*tRe_2 - ((xM3Im)*tIm_2)); // x3re
             out[(3+i)*2+1] = xM1Im - ((xM3Re)*tIm_2 + ((xM3Im)*tRe_2)); // x3im
             //console.log( (s + k + d*0), (s + k + d*1), (s + k + d*2), (s + k + d*3) );
-            console.log( j1, j2 );
+            //console.log( j1, j2 );
             k += 1; k = k%d;
-
-
+            
+            
             console.log( "-----------------------------" );
             //if( (i+4) % (z) == 0 ){ s += z; }
             if( (i+4) % (z) == 0 ){ s += z; }
