d3321d2536477cba80ef6b63707ad9163b3186a1
sch1z0net <eindudevommond@gmail.com>
2024-03-05 09:35:55 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -915,13 +915,13 @@ function fftComplexInPlace_seq_4(out) {
         console.log( "-size "+size2+"########################################" );
         for(let b = 0; b < r*4; b+=r){
           k = 0;
+          s = b;
           for(let i_ = 0; i_ < 4; i_++){
             let i = b + i_*r_;
             jk = i;
             jj = i;
             ji = i;
             w = 0;
-            s = b;
 
             let k_a = k; k += 1; k = k%d;
             let k_b = k; k += 1; k = k%d; 
