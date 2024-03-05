17f2a5c8d347171b024ad540dff5bec1854eab0c
sch1z0net <eindudevommond@gmail.com>
2024-03-05 12:58:06 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -892,20 +892,31 @@ function fftComplexInPlace_seq_4(out) {
     console.log();
     console.log();
     console.log();
-    let r = N; //64          // 4 16 64
-    let steps = (bits>>1);   
-    let ts = 1 << (steps-1); // 1  2  4
+    let r = N; //64          // 4   16   64  128
+    let steps = (bits>>1);   // 1    2    3    4   
+    let ts = 1 << (steps-1); // 1    2    4    8
     for(let p = 0; p < steps; p++){
         let d = 1<<(2*p);
         
-        //   p =      0     1    2    3
+        //             4   16   64  256
+        //   p =       0    1    2    3
+        //N=4   ->     -               
+        //N=16  ->     4-    4          
+        //N=64  ->     16-   16    4     
+        //N=256 ->     32-   32   16    4
+        if( p <= 1 ){ 
+          if( N <= 64 ){
+             r = (r >> 2); 
+          }else{
+             r = (r >> 2) / 2; 
+          }
+        }
+        if( p == 2 ){ 
+           r = (r >> 2);
+        }
+
         //N=4   ->     -
-        //N=16  ->     -    8
-        //N=64  ->     -   16    4
-        //N=256 ->     -   32   16    4
-        r = (p == 0 ? (r >> 2) : (N/(ts)) );
-        //N=4   ->     -
-        //N=16  ->     -    4    
+        //N=16  ->     -    8    
         //N=64  ->     -    4   16   
         //N=256 ->     -    4   16   
         let r_ = (p == 0 ? 4 : 4<<2*(p-1));           
