0dbafea87aa16ac74217e646f031114c31c0524f
sch1z0net <eindudevommond@gmail.com>
2024-03-06 14:31:44 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -978,10 +978,10 @@ function fftComplexInPlace_seq_4(out) {
             let i_d0 = (s + k_d + d*0); let i_d1 = (s + k_d + d*1); let i_d2 = (s + k_d + d*2); let i_d3 = (s + k_d + d*3);
             */
 
-            let i_a0 = (i + (r_%size1)*0); let i_a1 = (i + (r_%size1)*1); let i_a2 = (i + (r_%size1)*2); let i_a3 = (i + (r_%size1)*3);
-            let i_b0 = (i + (r_%size1)*0); let i_b1 = (i + (r_%size1)*1); let i_b2 = (i + (r_%size1)*2); let i_b3 = (i + (r_%size1)*3);
-            let i_c0 = (i + (r_%size1)*0); let i_c1 = (i + (r_%size1)*1); let i_c2 = (i + (r_%size1)*2); let i_c3 = (i + (r_%size1)*3);
-            let i_d0 = (i + (r_%size1)*0); let i_d1 = (i + (r_%size1)*1); let i_d2 = (i + (r_%size1)*2); let i_d3 = (i + (r_%size1)*3);
+            let i_a0 = (i + (r_%size2)*0); let i_a1 = (i + (r_%size2)*1); let i_a2 = (i + (r_%size2)*2); let i_a3 = (i + (r_%size2)*3);
+            let i_b0 = (i + (r_%size2)*0); let i_b1 = (i + (r_%size2)*1); let i_b2 = (i + (r_%size2)*2); let i_b3 = (i + (r_%size2)*3);
+            let i_c0 = (i + (r_%size2)*0); let i_c1 = (i + (r_%size2)*1); let i_c2 = (i + (r_%size2)*2); let i_c3 = (i + (r_%size2)*3);
+            let i_d0 = (i + (r_%size2)*0); let i_d1 = (i + (r_%size2)*1); let i_d2 = (i + (r_%size2)*2); let i_d3 = (i + (r_%size2)*3);
             
             let sign2a = (jj%size2 < size1)      ? 1 : -1;
             let sign1a = (jj%size1 < (size1>>1)) ? 1 : -1; 
