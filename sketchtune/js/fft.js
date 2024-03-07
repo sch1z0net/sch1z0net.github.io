17f9afca8ca208928423d5cc9c042e701966096c
sch1z0net <eindudevommond@gmail.com>
2024-03-07 23:06:34 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -1045,10 +1045,10 @@ function fftComplexInPlace_seq_4(out) {
           //let Tx0cIm = x1cRe;
           //let Tx2cRe = 0;
           //let Tx2cIm = x3cRe;
-          xM0ReC = x0cRe;
-          xM0ImC = x0cIm - x1cRe;  
-          xM2ReC = x2cRe;
-          xM2ImC = x2cIm - x3cRe;
+          //xM0ReC = x0cRe;
+          //xM0ImC = - x1cRe;  
+          //xM2ReC = x2cRe;
+          //xM2ImC = - x3cRe;
 
           xM1ReD = x0dRe - Tx1dRe;
           xM1ImD = x0dIm - Tx1dIm;
@@ -1059,11 +1059,8 @@ function fftComplexInPlace_seq_4(out) {
           out[( 4+i)*2+1] = xM2ReA; 
           out[( 5+i)*2+0] = xM1ReB + ((xM3ReB)*tRe_2f - ((xM3ImB)*tIm_2f));
           out[( 5+i)*2+1] = xM1ImB + ((xM3ReB)*tIm_2f + ((xM3ImB)*tRe_2f)); 
-          out[( 6+i)*2+0] =   x0cRe + ((xM2ReC)*tRe_2g - ((xM2ImC)*tIm_2g));
-          out[( 6+i)*2+1] = - x1cRe + ((xM2ReC)*tIm_2g + ((xM2ImC)*tRe_2g));
-          //out[(6+i)*2+0] = x0cRe  + (x2cRe*tRe_2g);
-          //out[(6+i)*2+1] =          (x2cRe*tIm_2g);
-
+          out[( 6+i)*2+0] =   x0cRe + ((x2cRe)*tRe_2g - ((-x3cRe)*tIm_2g));
+          out[( 6+i)*2+1] = - x1cRe + ((x2cRe)*tIm_2g + ((-x3cRe)*tRe_2g));
           out[( 7+i)*2+0] = xM1ReD + ((xM3ReD)*tRe_2h - ((xM3ImD)*tIm_2h));
           out[( 7+i)*2+1] = xM1ImD + ((xM3ReD)*tIm_2h + ((xM3ImD)*tRe_2h));
 
