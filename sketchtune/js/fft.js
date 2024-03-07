ab968795c95ed27fb4471ae1355f8ee6b23b68ec
sch1z0net <eindudevommond@gmail.com>
2024-03-07 12:07:37 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -847,6 +847,9 @@ let steps = 4;           // 1    2    3    4
 let ts    = 8;           // 1    2    4    8
 let tsq   = 64;
 
+function fftComplexInPlace_seq_4(out) {
+    
+
     let x0aRe, x0aIm; let x1aRe, x1aIm; let x2aRe, x2aIm; let x3aRe, x3aIm;
     let x0bRe, x0bIm; let x1bRe, x1bIm; let x2bRe, x2bIm; let x3bRe, x3bIm;
     let x0cRe, x0cIm; let x1cRe, x1cIm; let x2cRe, x2cIm; let x3cRe, x3cIm;
@@ -864,11 +867,12 @@ let tsq   = 64;
     let tRe_2b; let tIm_2b;
     let tRe_2c; let tIm_2c;    
     let tRe_2d; let tIm_2d;
+    
 
-function fftComplexInPlace_seq_4(out) {
     let pre1 = 0;
     let pre2 = 2;
     let s = 1;
+    let k = 0;
     for(let p = 0, s0 = 1, s1 = 2, s2 = 4; p < steps; p++, s0 <<= 2, s1 <<= 2, s2 <<= 2) {
     //for (let p = 0; p < steps; p++, s <<= 2) {
         
@@ -888,7 +892,7 @@ function fftComplexInPlace_seq_4(out) {
         let b = 0;
         */
 
-        let k = p==0 ? 1 : (1<<(2*p));  //  1  4  16  64
+        //let k = p==0 ? 1 : (1<<(2*p));  //  1  4  16  64
 
         //console.log( "-size "+size2+"########################################" );
         /*for(let b_ = 0; b_ < ts; b_++){
