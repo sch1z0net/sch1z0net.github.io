6ce98b23694da71e63baf316900fecbef87f1fc3
sch1z0net <eindudevommond@gmail.com>
2024-03-07 11:15:39 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -843,10 +843,12 @@ function fftComplexInPlace_seq(out) {
 }*/
 
 let ____F = LOOKUP_RADIX2_256;
+let steps = 4;           // 1    2    3    4   
+let ts    = 8;           // 1    2    4    8
+let tsq   = 64;
 
 function fftComplexInPlace_seq_4(out) {
-    const N = out.length/2;
-    const bits = Math.log2(N);
+    const N = 256;
 
     /*
     let ____F;
@@ -903,12 +905,8 @@ function fftComplexInPlace_seq_4(out) {
     let tRe_2d; let tIm_2d;
     */
 
-    let r = N; //64          // 4   16   64  128
-    let steps = (bits>>1);   // 1    2    3    4   
-    let ts = 1 << (steps-1); // 1    2    4    8
-    let tsq = ts*ts;
-    let pre1 = 0;
-    let pre2 = 2;
+    //let pre1 = 0;
+    //let pre2 = 2;
     let s = 1;
     //for(let p = 0, s0 = 1, s1 = 2, s2 = 4; p < steps; p++, s0 <<= 2, s1 <<= 2, s2 <<= 2) {
     for (let p = 0; p < steps; p++, s <<= 2) {
