62a1031063bb205917bbf3a3a13e60fbef288bea
sch1z0net <eindudevommond@gmail.com>
2024-03-07 10:58:37 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -910,7 +910,7 @@ function fftComplexInPlace_seq_4(out) {
     let pre1 = 0;
     let pre2 = 2;
     for(let p = 0, s0 = 1, s1 = 2, s2 = 4; p < steps; p++, s0 <<= 2, s1 <<= 2, s2 <<= 2) {
-        
+        /*
         let r = 4;  
             if(p==0){ r = 32; }
             if(p==1){ r = 32; }
@@ -926,6 +926,7 @@ function fftComplexInPlace_seq_4(out) {
         let b = 0;
 
         let k = p==0 ? 1 : (1<<(2*p));  //  1  4  16  64
+        */
 
         //console.log( "-size "+size2+"########################################" );
         for(let b_ = 0; b_ < ts*ts; b_++){
@@ -1096,8 +1097,8 @@ function fftComplexInPlace_seq_4(out) {
             // //console.log( "-----------------------------" );
           
         } 
-        pre1 += (2 << 2*p) + (2 << 2*p+1);
-        pre2 += (2 << 2*p+1) + (2 << 2*p+2);
+        //pre1 += (2 << 2*p) + (2 << 2*p+1);
+        //pre2 += (2 << 2*p+1) + (2 << 2*p+2);
 
         
         /*for(let i=0; i<N; i++){
