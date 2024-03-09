7a0d1285709cc53cddfc165876a77ddd342710a4
sch1z0net <eindudevommond@gmail.com>
2024-03-09 19:27:38 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -961,11 +961,18 @@ function fftComplexInPlace_seq_4(out) {
     tIm_1d  =   tRe_1b;
 
     tRe_2b  = ____F[14+(1*2+0)]; 
+    tIm_2b  = ____F[14+(1*2+1)];
     tRe_2c  = ____F[14+(2*2+0)]; 
-    tRe_2d  = ____F[14+(3*2+0)];
-    tIm_2b  =   tRe_2d;
-    tIm_2c  =   tRe_2c; 
-    tIm_2d  =   tRe_2b;
+    tIm_2c  = ____F[14+(2*2+1)];
+    tRe_2d  = ____F[14+(3*2+0)]; 
+    tIm_2d  = ____F[14+(3*2+1)];
+
+    tRe_2f  = ____F[14+(5*2+0)]; 
+    tIm_2f  = ____F[14+(5*2+1)];
+    tRe_2g  = ____F[14+(6*2+0)]; 
+    tIm_2g  = ____F[14+(6*2+1)];
+    tRe_2h  = ____F[14+(7*2+0)]; 
+    tIm_2h  = ____F[14+(7*2+1)];
 
     for(let i_ = 0; i_ < tsq; i_+=4){
           let i = i_ * 4;
@@ -1035,8 +1042,8 @@ function fftComplexInPlace_seq_4(out) {
           xM0ReA = x0aRe - x1aRe;              
           xM2ReA = x2aRe - x3aRe;
 
-          out[( 4+i)*2+0] = xM0ReA; 
-          out[( 4+i)*2+1] = xM2ReA; 
+          out[( 4+i)*2+0] =   xM0ReA; 
+          out[( 4+i)*2+1] =   xM2ReA; 
           out[( 5+i)*2+0] =   out[(11+i)*2+0];
           out[( 5+i)*2+1] = - out[(11+i)*2+1];
           out[( 6+i)*2+0] =   out[(10+i)*2+0];
