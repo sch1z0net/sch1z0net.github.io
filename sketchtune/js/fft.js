b0edc1d298ada3c786edbb951932a039d45ac140
sch1z0net <eindudevommond@gmail.com>
2024-03-09 23:24:30 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -3558,7 +3558,7 @@ function compareFFTResults(array1, array2) {
 
 /****************** TEST SPEED *******************/ 
 
-measureTime(256,4);
+//measureTime(256,4);
 //measureTime(512,5);
 //measureTime(512,6);
 //measureTime(512,7);
