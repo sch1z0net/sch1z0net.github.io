1600d742e7cbb24ae608385bab5058a7fd2281e3
sch1z0net <eindudevommond@gmail.com>
2024-03-10 21:58:40 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -1302,10 +1302,10 @@ function fftComplexInPlace_seq_4(out) {
     //
 
         const halfSize = 64;
-        for (let i = 0; i < 256; i += 128) {
+        //for (let i = 0; i < 256; i += 128) {
             for (let j = 0; j < halfSize; j++) {
-                const evenIndex = i + j;
-                const oddIndex = i + j + halfSize;
+                const evenIndex = 0 + j;
+                const oddIndex = 0 + j + halfSize;
                 const evenPartRe = out[evenIndex * 2    ];
                 const evenPartIm = out[evenIndex * 2 + 1];
                 const oddPartRe  = out[oddIndex  * 2    ];
@@ -1326,7 +1326,17 @@ function fftComplexInPlace_seq_4(out) {
                 out[oddIndex  * 2    ] = evenPartRe - twiddledOddRe;
                 out[oddIndex  * 2 + 1] = evenPartIm - twiddledOddIm;
             }
-        }
+            for (let j = 0; j < halfSize; j++) {
+                const evenIndex = 128 + j;
+                const oddIndex = 128 + j + halfSize;
+
+                // Combine results of even and odd parts in place
+                out[evenIndex * 2    ] = out[512 - (evenIndex * 2     )]
+                out[evenIndex * 2 + 1] = out[512 - (evenIndex * 2  + 1)]
+                out[oddIndex  * 2    ] = out[512 - (oddIndex  * 2     )]
+                out[oddIndex  * 2 + 1] = out[512 - (oddIndex  * 2  + 1)]
+            }  
+        //}
     
 
 
