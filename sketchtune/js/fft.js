3d3f3912136d1595a80f1c9d6e5c064d0fdf5a7b
sch1z0net <eindudevommond@gmail.com>
2024-03-11 19:46:00 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -973,56 +973,49 @@ for (let idx = 0; idx < 2048; idx += 32) {
     // Unrolled loop iteration 1
     let x0aRe_1 = out[idx];
     let x1aRe_1 = out[idx + 2];
-    let x2aRe_1 = out[idx + 4];
-    let x3aRe_1 = out[idx + 6];
-
     out[idx + 2] = x0aRe_1 - x1aRe_1;
+    let x3aRe_1 = out[idx + 6];
     out[idx + 6] = x0aRe_1 - x1aRe_1;
 
-    out[idx    ] = x0aRe_1 + x1aRe_1 + x2aRe_1 + x3aRe_1;
+    let x2aRe_1 = out[idx + 4];
     out[idx + 4] = x0aRe_1 + x1aRe_1 - x2aRe_1 - x3aRe_1; 
     out[idx + 3] = x2aRe_1 - x3aRe_1;
+    out[idx    ] = x0aRe_1 + x1aRe_1 + x2aRe_1 + x3aRe_1;
     out[idx + 7] = -x2aRe_1 + x3aRe_1;
 
     // Unrolled loop iteration 2
     let x0aRe_2 = out[idx + 8];
     let x1aRe_2 = out[idx + 10];
-    let x2aRe_2 = out[idx + 12];
-    let x3aRe_2 = out[idx + 14];
-
     out[idx + 10] = x0aRe_2 - x1aRe_2;
+    let x3aRe_2 = out[idx + 14];
     out[idx + 14] = x0aRe_2 - x1aRe_2;
-
-    out[idx +  8] = x0aRe_2 + x1aRe_2 + x2aRe_2 + x3aRe_2;
+    let x2aRe_2 = out[idx + 12];
     out[idx + 12] = x0aRe_2 + x1aRe_2 - x2aRe_2 - x3aRe_2; 
     out[idx + 11] = x2aRe_2 - x3aRe_2;
+    out[idx +  8] = x0aRe_2 + x1aRe_2 + x2aRe_2 + x3aRe_2;
     out[idx + 15] = -x2aRe_2 + x3aRe_2;
 
     // Unrolled loop iteration 3
     let x0aRe_3 = out[idx + 16];
     let x1aRe_3 = out[idx + 18];
-    let x2aRe_3 = out[idx + 20];
-    let x3aRe_3 = out[idx + 22];
-
     out[idx + 18] = x0aRe_3 - x1aRe_3;
+    let x3aRe_3 = out[idx + 22];
     out[idx + 22] = x0aRe_3 - x1aRe_3;
-
-    out[idx + 16] = x0aRe_3 + x1aRe_3 + x2aRe_3 + x3aRe_3;
+    let x2aRe_3 = out[idx + 20];
     out[idx + 20] = x0aRe_3 + x1aRe_3 - x2aRe_3 - x3aRe_3; 
+    out[idx + 16] = x0aRe_3 + x1aRe_3 + x2aRe_3 + x3aRe_3;
     out[idx + 19] = x2aRe_3 - x3aRe_3;
     out[idx + 23] = -x2aRe_3 + x3aRe_3;
 
     // Unrolled loop iteration 4
     let x0aRe_4 = out[idx + 24];
     let x1aRe_4 = out[idx + 26];
-    let x2aRe_4 = out[idx + 28];
-    let x3aRe_4 = out[idx + 30];
-
     out[idx + 26] = x0aRe_4 - x1aRe_4;
+    let x3aRe_4 = out[idx + 30];
     out[idx + 30] = x0aRe_4 - x1aRe_4;
-
-    out[idx + 24] = x0aRe_4 + x1aRe_4 + x2aRe_4 + x3aRe_4;
+    let x2aRe_4 = out[idx + 28];
     out[idx + 28] = x0aRe_4 + x1aRe_4 - x2aRe_4 - x3aRe_4; 
+    out[idx + 24] = x0aRe_4 + x1aRe_4 + x2aRe_4 + x3aRe_4;
     out[idx + 27] = x2aRe_4 - x3aRe_4;
     out[idx + 31] = -x2aRe_4 + x3aRe_4;
 }
