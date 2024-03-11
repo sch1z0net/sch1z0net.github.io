a6f450c54968e794605cd95c3101c3148c70d109
sch1z0net <eindudevommond@gmail.com>
2024-03-11 23:01:47 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -2045,7 +2045,7 @@ function fftComplexInPlace_seq_4(realInput) {
             const evenIndex = j;
             const oddIndex  = j + 128;
 
-            if(j >= 64){
+            if(j > 64){
               out[evenIndex * 2]     =  out[512 - evenIndex * 2] ;
               out[evenIndex * 2 + 1] = -out[512 - evenIndex * 2 + 1];
               out[oddIndex * 2]      =  out[512 - oddIndex * 2];
