0dc5e0fee8775713dc7386a0a971956bda87aba6
sch1z0net <eindudevommond@gmail.com>
2024-03-15 23:43:32 +0100

    mhh maybe looked wrong...

--- sketchtune/js/fft_float.c
+++ sketchtune/js/fft_float.c
@@ -1330,9 +1330,9 @@ inputBR1024[1023]=paddedInput[1023];
     // FFT step for SIZE 4
     ////////////////////////////////////////////////
     // Call SIMD function
-    simd_compute_4(inputBR1024, out1024);
+    //simd_compute_4(inputBR1024, out1024);
 
-    /*
+    
     for (int idx = 0, out_idx = 0; idx < 1024; idx += 4, out_idx += 8) {
         float x0aRe = inputBR1024[idx];
         float x1aRe = inputBR1024[idx + 1];
@@ -1353,7 +1353,7 @@ inputBR1024[1023]=paddedInput[1023];
         out1024[out_idx + 6] = diff1;
         out1024[out_idx + 7] = -diff2;
     }
-    */
+    
 
     ////////////////////////////////////////////////
     ////////////////////////////////////////////////
