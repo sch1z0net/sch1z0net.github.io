d8d2fc4a31210a04fd4eae9c480acdffa16f96f2
sch1z0net <eindudevommond@gmail.com>
2024-03-16 23:42:02 +0100

    add title

--- oink_fft/js/main.js
+++ oink_fft/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-
-   
-
+    // Create a title element
+    var $title = $("<h1>").text("OINK FFT").attr("id", "title");
+    // Append the title element to the body
+    $("#root").append($title);
 });
