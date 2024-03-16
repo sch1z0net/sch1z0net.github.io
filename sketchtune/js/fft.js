1c183d7cc525ea04b323ea74be09dd7f6b8f93f9
sch1z0net <eindudevommond@gmail.com>
2024-03-16 15:21:11 +0100

    Update fft.js

--- sketchtune/js/fft.js
+++ sketchtune/js/fft.js
@@ -635,7 +635,7 @@ const measureTime = (fftSize) => {
 
 function runPerformance(){
         console.log("\n\nPerformance Test:");
-        
+
         measureTime(1024);
         measureTime(1024);
         measureTime(1024);
@@ -720,10 +720,10 @@ function runComparison(){
 
 function runForthAndBack(){
     console.log("\n\nCompare after Forth and Back:");
-    let signal128  = computeInverseFFTonHalf128(fftReal128(testData128.slice(0, 128));
-    let signal256  = computeInverseFFTonHalf256(fftReal256(testData256.slice(0, 256));
-    let signal512  = computeInverseFFTonHalf512(fftReal512(testData512.slice(0, 512));
-    let signal1024 = computeInverseFFTonHalf1024(fftReal1024(testData1024.slice(0, 1024));
+    let signal128  = computeInverseFFTonHalf128(fftReal128(testData128.slice(0, 128)));
+    let signal256  = computeInverseFFTonHalf256(fftReal256(testData256.slice(0, 256)));
+    let signal512  = computeInverseFFTonHalf512(fftReal512(testData512.slice(0, 512)));
+    let signal1024 = computeInverseFFTonHalf1024(fftReal1024(testData1024.slice(0, 1024)));
 
     console.log("128:   ",compareFFTResults(testData128, signal128));
     console.log("256:   ",compareFFTResults(testData256, signal256));
