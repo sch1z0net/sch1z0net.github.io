341d16f61d9983a5b433f75bf37130bf207d95ef
sch1z0net <eindudevommond@gmail.com>
2024-03-17 20:03:08 +0100

    Update main.js

--- oink_fft/js/main.js
+++ oink_fft/js/main.js
@@ -11,46 +11,47 @@ var $tbody;
 var $loading;
 var $descr_div;
 
+function resetPerformanceTable(){
+    $stats_div.empty();
+}
+
+function createPerformanceTable(){
+    resetPerformanceTable();
+
+    // Create a table element
+    var $table = $("<table>").attr("id", "fft-table");
+    // Create table header
+    var $thead = $("<thead>").appendTo($table);
+    // Create the loading circle element
+    $loading.show().appendTo($thead);
+    var $trHead = $("<tr>").appendTo($thead);
+    // Create Header Text
+    $("<th>").text("FFT Performance (measured in OINKS per second)").attr("colspan", 6).appendTo($trHead); // colspan to span all columns
+    // Create table body
+    $tbody = $("<tbody>").appendTo($table);
+
+    // HEADER
+    var $tr_sizes = $("<tr>").attr("id", "tr_header").appendTo($tbody); 
+    $("<td>").text("FFT size").appendTo($tr_sizes);
+    for (var size = 128; size <= 1024; size *= 2) {
+        $("<td>").text(size).appendTo($tr_sizes);
+    }
+
+    // Append the table to the body
+    $stats_div.append($table);
+}
+
+function addPerformanceRow(name, results){
+    var $tr = $("<tr>");
+    $("<td>").text(name).appendTo($tr);
+    for (var size = 128; size <= 1024; size *= 2) {
+        $("<td>").text( results.get(size) ).appendTo($tr);
+    }
+    $tr.appendTo($tbody);
+}
+
+
 $(document).ready(function(){
-    function resetPerformanceTable(){
-        $stats_div.empty();
-    }
-
-    function createPerformanceTable(){
-        resetPerformanceTable();
-
-        // Create a table element
-        var $table = $("<table>").attr("id", "fft-table");
-        // Create table header
-        var $thead = $("<thead>").appendTo($table);
-        // Create the loading circle element
-        $loading.show().appendTo($thead);
-        var $trHead = $("<tr>").appendTo($thead);
-        // Create Header Text
-        $("<th>").text("FFT Performance (measured in OINKS per second)").attr("colspan", 6).appendTo($trHead); // colspan to span all columns
-        // Create table body
-        $tbody = $("<tbody>").appendTo($table);
-
-        // HEADER
-        var $tr_sizes = $("<tr>").attr("id", "tr_header").appendTo($tbody); 
-        $("<td>").text("FFT size").appendTo($tr_sizes);
-        for (var size = 128; size <= 1024; size *= 2) {
-            $("<td>").text(size).appendTo($tr_sizes);
-        }
-
-        // Append the table to the body
-        $stats_div.append($table);
-    }
-
-    function addPerformanceRow(name, results){
-        var $tr = $("<tr>");
-        $("<td>").text(name).appendTo($tr);
-        for (var size = 128; size <= 1024; size *= 2) {
-            $("<td>").text( results.get(size) ).appendTo($tr);
-        }
-        $tr.appendTo($tbody);
-    }
-
     $title_div = $("<div>").attr("id", "title_div");
     $title = $("<h1>").text("OINK FFT").attr("id", "title");
     $subtitle = $("<h2>").text("the oinkiest FFT in the web").attr("id", "subtitle");
