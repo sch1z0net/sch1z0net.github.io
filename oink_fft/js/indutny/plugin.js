d0b490482705e737d7f1600cd7581237a3487b29
sch1z0net <eindudevommond@gmail.com>
2024-03-20 21:09:59 +0100

    test

--- oink_fft/js/indutny/plugin.js
+++ oink_fft/js/indutny/plugin.js
@@ -1,3 +1,5 @@
+import * as INDUTNY_ from '/oink_fft/js/indutny/fft.js';
+
 //////////////////////////////////////
 //////////////////////////////////////
 // PREPARE AND PERFORM INDUTNY
@@ -10,15 +12,15 @@ let indutny_f_2048, indutny_out_2048;
 
 function initializeINDUTNY(){
     return new Promise((resolve, reject) => {
-        indutny_f_128 = new IND_FFT(128);
+        indutny_f_128 = new INDUTNY_.IND_FFT(128);
         indutny_out_128 = indutny_f_128.createComplexArray();
-        indutny_f_256 = new IND_FFT(256);
+        indutny_f_256 = new INDUTNY_.IND_FFT(256);
         indutny_out_256 = indutny_f_256.createComplexArray();
-        indutny_f_512 = new IND_FFT(512);
+        indutny_f_512 = new INDUTNY_.IND_FFT(512);
         indutny_out_512 = indutny_f_512.createComplexArray();
-        indutny_f_1024 = new IND_FFT(1024);
+        indutny_f_1024 = new INDUTNY_.IND_FFT(1024);
         indutny_out_1024 = indutny_f_1024.createComplexArray();
-        indutny_f_2048 = new IND_FFT(2048);
+        indutny_f_2048 = new INDUTNY_.IND_FFT(2048);
         indutny_out_2048 = indutny_f_2048.createComplexArray();
         resolve();
     });
@@ -34,7 +36,7 @@ const perform_INDUTNY = (instance, out, testData) => {
 //////////////////////////////////////
 // IMPLEMENT GIVEN INTERFACE
 //////////////////////////////////////
-export const PLUGIN_INDUTNY = {
+const PLUGIN_INDUTNY = {
   idname:   function() { return "INDUTNY" },
   fullname: function() { return "FFT.JS (indunty)"},
   url:      function() { return "https://github.com/indutny/fft.js/" },
@@ -48,6 +50,7 @@ export const PLUGIN_INDUTNY = {
   example:  function(testData) { return perform_INDUTNY(indutny_f_1024,  indutny_out_1024,  testData); },
 };
 
+export default PLUGIN_INDUTNY;
 
 // Test if PLUGIN implements the INTERFACE
 //console.log(implementsInterface(PLUGIN_INDUTNY, interface));
