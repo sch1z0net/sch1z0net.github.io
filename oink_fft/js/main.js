31afd31d11ba93c988877ac21b431abc554eff0e
sch1z0net <eindudevommond@gmail.com>
2024-03-21 15:17:44 +0100

    test

--- oink_fft/js/main.js
+++ oink_fft/js/main.js
@@ -366,7 +366,7 @@ $(document).ready(function(){
     $descr_div.text("According to ChatGPT, OINK FFT stands for: Outrageously Insane, Notoriously Quick Fast Fourier Transform!");
 
     
-    let $intro = $("<div>RUN PERFORMANCE TEST</div>");
+    let $intro = $("<div class='pulse'>RUN PERFORMANCE TEST</div>");
     $tab_intro.append($intro);
     
 
