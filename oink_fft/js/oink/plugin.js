0559f16a2cab14552411869a93d762ad385e65fd
sch1z0net <eindudevommond@gmail.com>
2024-03-21 11:16:44 +0100

    Update plugin.js

--- oink_fft/js/oink/plugin.js
+++ oink_fft/js/oink/plugin.js
@@ -26,7 +26,7 @@ const PLUGIN_OINK = {
   example:  function() { return perform_OINK(fftReal1024, testData.slice()).slice(); },
   init: async function() {
     const Module_OINK = await import('/oink_fft/js/oink/fft_wasm.js');
-    _Module_OINK_ = await Module_OINK.default();
+    const _Module_OINK_ = await Module_OINK.default();
     await initializeModuleOINK(_Module_OINK_);
   },
   fft128:   function(testData) { perform_OINK(fftReal128,  testData); },
