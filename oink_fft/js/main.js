37b89a314221937ec54281a2169f084286d28fa4
sch1z0net <eindudevommond@gmail.com>
2024-03-21 16:51:57 +0100

    Update main.js

--- oink_fft/js/main.js
+++ oink_fft/js/main.js
@@ -504,7 +504,7 @@ $(document).ready(async function(){
     await setup(FFT_BANK);
 
     await $tab_intro.click(function(){
-         $tab_intro.hide(); 
+         $tab_intro.fadeOut(2000);
          $reload.trigger('click');
     });
 });
