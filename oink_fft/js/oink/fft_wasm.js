7d026adba8cebb79834dc3cee55c076905f7c28a
sch1z0net <eindudevommond@gmail.com>
2024-03-22 03:32:31 +0100

    Update fft_wasm.js

--- oink_fft/js/oink/fft_wasm.js
+++ oink_fft/js/oink/fft_wasm.js
@@ -1,5 +1,6 @@
 var Module_OINK = (() => {
-  var _scriptDir = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : undefined;
+  //var _scriptDir = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : undefined;
+  var _scriptDir = "https://cdn.jsdelivr.net/gh/sch1z0net/oink@v0.1.3-alpha/"
   if (typeof __filename !== 'undefined') _scriptDir ||= __filename;
   return (
 function(moduleArg = {}) {
