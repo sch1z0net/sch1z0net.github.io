ea8aa3860d846a29dd647d9c624f697ea6bd0cc3
sch1z0net <eindudevommond@gmail.com>
2024-03-22 21:59:45 +0100

    Update setup.js

--- oink_fft/js/setup.js
+++ oink_fft/js/setup.js
@@ -1,11 +1,11 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 import registerPlugin from './interface.js'
-import PLUGIN_INDUTNY from '.fft_libs/indutny/plugin.js'
-import PLUGIN_DSP     from '.fft_libs/dsp/plugin.js'
-import PLUGIN_OOURA   from '.fft_libs/ooura/plugin.js'
-import PLUGIN_KISS    from '.fft_libs/kiss/plugin.js'
-import PLUGIN_OINK    from '.fft_libs/oink/plugin.js'
+import PLUGIN_INDUTNY from './fft_libs/indutny/plugin.js'
+import PLUGIN_DSP     from './fft_libs/dsp/plugin.js'
+import PLUGIN_OOURA   from './fft_libs/ooura/plugin.js'
+import PLUGIN_KISS    from './fft_libs/kiss/plugin.js'
+import PLUGIN_OINK    from './fft_libs/oink/plugin.js'
 
 async function setup(FFT_BANK){
     await registerPlugin(PLUGIN_INDUTNY, FFT_BANK);
