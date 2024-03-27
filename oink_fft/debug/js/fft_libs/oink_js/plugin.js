49afdd972d3ffa043d28ca1d792a2ccd313d4964
sch1z0net <eindudevommond@gmail.com>
2024-03-27 11:32:19 +0100

    Update plugin.js

--- oink_fft/debug/js/fft_libs/oink_js/plugin.js
+++ oink_fft/debug/js/fft_libs/oink_js/plugin.js
@@ -3,13 +3,13 @@
 // IMPORT THE FFT LIBRARY
 //////////////////////////////////////
 //import * as OINK from "https://cdn.jsdelivr.net/gh/sch1z0net/oink@v0.1.5-alpha/oink_fft.js";
-import * as OINK from "./oink_fft.js"
+import * as OINK_js from "./oink_fft.js"
 //////////////////////////////////////
 //////////////////////////////////////
 // PREPARE AND PERFORM KISS
 //////////////////////////////////////
 
-const perform_OINK = (instance, testData) => {
+const perform_OINK_js = (instance, testData) => {
     return instance(testData.slice());
 };
 
@@ -17,19 +17,19 @@ const perform_OINK = (instance, testData) => {
 //////////////////////////////////////
 // IMPLEMENT GIVEN INTERFACE
 //////////////////////////////////////
-const PLUGIN_OINK = {
-  idname:   function() { return "OINK" },
-  fullname: function() { return "OINK (sch1z0net)"},
+const PLUGIN_OINK_js = {
+  idname:   function() { return "OINK_js" },
+  fullname: function() { return "OINK_js (sch1z0net)"},
   url:      function() { return "https://github.com/sch1z0net/oink" },
   precision:function() { return "float" },
-  example:  function() { return perform_OINK(OINK.fftReal1024, testData.slice()).slice(); },
+  example:  function() { return perform_OINK_js(OINK_js.fftReal1024, testData.slice()).slice(); },
   init: async function() { },
-  fft128:   function(testData) { perform_OINK(OINK.fftReal128,  testData); },
-  fft256:   function(testData) { perform_OINK(OINK.fftReal256,  testData); },
-  fft512:   function(testData) { perform_OINK(OINK.fftReal512,  testData); },
-  fft1024:  function(testData) { perform_OINK(OINK.fftReal1024, testData); },
-  fft2048:  function(testData) { perform_OINK(OINK.fftReal2048, testData); },
-  example:  function(testData) { return perform_OINK(OINK.fftReal1024,  testData); },
+  fft128:   function(testData) { perform_OINK_js(OINK_js.fftReal128,  testData); },
+  fft256:   function(testData) { perform_OINK_js(OINK_js.fftReal256,  testData); },
+  fft512:   function(testData) { perform_OINK_js(OINK_js.fftReal512,  testData); },
+  fft1024:  function(testData) { perform_OINK_js(OINK_js.fftReal1024, testData); },
+  fft2048:  function(testData) { perform_OINK_js(OINK_js.fftReal2048, testData); },
+  example:  function(testData) { return perform_OINK_js(OINK.fftReal1024,  testData); },
 };
 
 
@@ -37,4 +37,4 @@ const PLUGIN_OINK = {
 //////////////////////////////////////
 // EXPORT
 //////////////////////////////////////
-export default PLUGIN_OINK;
+export default PLUGIN_OINK_js;
