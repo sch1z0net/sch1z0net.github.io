5d408dcda33d0555cfc991c957d9ae66672b5e98
sch1z0net <eindudevommond@gmail.com>
2024-03-30 10:05:57 +0100

    test

--- oink_fft/debug/js/setup.js
+++ oink_fft/debug/js/setup.js
@@ -9,12 +9,13 @@ import PLUGIN_OINK    from './fft_libs/oink/plugin.js'
 import PLUGIN_OINK_js from './fft_libs/oink_js/plugin.js'
 
 async function setup(FFT_BANK){
+    await registerPlugin(PLUGIN_OINK_js, FFT_BANK);
     await registerPlugin(PLUGIN_INDUTNY, FFT_BANK);
     await registerPlugin(PLUGIN_DSP, FFT_BANK);
     await registerPlugin(PLUGIN_OOURA, FFT_BANK);
     await registerPlugin(PLUGIN_KISS, FFT_BANK);
     await registerPlugin(PLUGIN_OINK, FFT_BANK);
-    await registerPlugin(PLUGIN_OINK_js, FFT_BANK);
+
 };
 
 export default setup;
