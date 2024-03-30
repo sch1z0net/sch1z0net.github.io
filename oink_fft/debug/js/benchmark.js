7608908743cf88db0e971dc3fd55adad88c9b723
sch1z0net <eindudevommond@gmail.com>
2024-03-30 12:00:11 +0100

    Update benchmark.js

--- oink_fft/debug/js/benchmark.js
+++ oink_fft/debug/js/benchmark.js
@@ -209,7 +209,7 @@ async function runAllPerformanceTests(FFT_BANK, PARAMS, charts){
     }
 }
 
-let testData8 = Float32Array([1.0, 0.5, 0.0, -0.5, -1.0, -0.5, 0.0, 0.5]);
+let testData8 = new Float32Array([1.0, 0.5, 0.0, -0.5, -1.0, -0.5, 0.0, 0.5]);
 
 function runErrorComparison(FFT_BANK, output_values){
     let testData = generateTestData(8);
