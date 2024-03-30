03840de9ae595fdaad932bd62849fdce4f31bc06
sch1z0net <eindudevommond@gmail.com>
2024-03-30 19:15:20 +0100

    Update fft_8_unrolled.js

--- oink_fft/debug/js/fft_libs/oink_js/ffts/fft_8_unrolled.js
+++ oink_fft/debug/js/fft_libs/oink_js/ffts/fft_8_unrolled.js
@@ -1,3 +1,9 @@
+let FFT_FAC_4 = new Float32Array([
+1.0000000000000000,0.0000000000000000,-0.0000000437113883,1.0000000000000000
+]);
+let FFT_FAC_8 = new Float32Array([
+1.0000000000000000,0.0000000000000000,0.7071067690849304,0.7071067690849304,-0.0000000437113883,1.0000000000000000,-0.7071067690849304,0.7071067690849304
+]);
 
 
 let iBR8 = new Float32Array(8);
@@ -16,7 +22,7 @@ function fftReal8(realInput) {
         _iP8 = realInput;
     }
 
-    /*
+
     //Bit Reversal
     {
         iBR8[0]=_iP8[0]; 
@@ -27,34 +33,68 @@ function fftReal8(realInput) {
         iBR8[5]=_iP8[5]; 
         iBR8[6]=_iP8[3]; 
         iBR8[7]=_iP8[7]; 
-    }*/
-    iBR8 = _iP8;
-
-    ////////////////////////////////////////////////
-    ////////////////////////////////////////////////
-    // RADIX 4 - FFT step for SIZE 4/8 
-    ////////////////////////////////////////////////
-
-    for (let idx = 0, out_idx = 0; idx < 8; idx += 4, out_idx += 8) {
-        let x0aRe = iBR8[idx    ];
-        let x1aRe = iBR8[idx + 1];
-        let x2aRe = iBR8[idx + 2];
-        let x3aRe = iBR8[idx + 3];
-
-        let sum1  = x0aRe + x1aRe;
-        let sum2  = x2aRe + x3aRe;
-        let diff1 = x0aRe - x1aRe;
-        let diff2 = (x2aRe - x3aRe) * 0.7071067690849304;
-
-        out8[out_idx]     = sum1 + sum2;
-        out8[out_idx + 1] = 0.0;
-        out8[out_idx + 2] = diff1 + diff2;
-        out8[out_idx + 3] =         diff2;
-        out8[out_idx + 4] = sum1 - sum2;
-        out8[out_idx + 5] = 0.0;
-        out8[out_idx + 6] = diff1 - diff2;
-        out8[out_idx + 7] =       - diff2;
     }
+
+    ////////////////////////////////////////////////
+    ////////////////////////////////////////////////
+    // RADIX 2 (rolled) - FFT step for SIZE 4 
+    ////////////////////////////////////////////////
+    { 
+     for (let j = 0; j < 2; j++) { 
+         let eI = 0 + j;
+         let oI = 0 + j + 2;
+         let eRe  = out8[eI * 2    ];
+         let eIm  = out8[eI * 2 + 1];
+         let oRe  = out8[oI * 2    ];
+         let oIm  = out8[oI * 2 + 1];
+         let tRe  = FFT_FAC_4[j * 2 + 0];
+         let tIm  = FFT_FAC_4[j * 2 + 1];
+         let t_oRe = oRe * tRe - oIm * tIm;
+         let t_oIm = oRe * tIm + oIm * tRe;
+         out8[eI * 2    ] = eRe + t_oRe;
+         out8[eI * 2 + 1] = eIm + t_oIm;
+         out8[oI * 2    ] = eRe - t_oRe;
+         out8[oI * 2 + 1] = eIm - t_oIm;
+     }
+     for (let j = 0; j < 2; j++) { 
+         let eI = 4 + j;
+         let oI = 4 + j + 2;
+         let eRe  = out8[eI * 2    ];
+         let eIm  = out8[eI * 2 + 1];
+         let oRe  = out8[oI * 2    ];
+         let oIm  = out8[oI * 2 + 1];
+         let tRe  = FFT_FAC_4[j * 2 + 0];
+         let tIm  = FFT_FAC_4[j * 2 + 1];
+         let t_oRe = oRe * tRe - oIm * tIm;
+         let t_oIm = oRe * tIm + oIm * tRe;
+         out8[eI * 2    ] = eRe + t_oRe;
+         out8[eI * 2 + 1] = eIm + t_oIm;
+         out8[oI * 2    ] = eRe - t_oRe;
+         out8[oI * 2 + 1] = eIm - t_oIm;
+     }
+    } 
+    ////////////////////////////////////////////////
+    ////////////////////////////////////////////////
+    // RADIX 2 (rolled) - FFT step for SIZE 8 
+    ////////////////////////////////////////////////
+    { 
+     for (let j = 0; j < 4; j++) { 
+         let eI = 0 + j;
+         let oI = 0 + j + 4;
+         let eRe  = out8[eI * 2    ];
+         let eIm  = out8[eI * 2 + 1];
+         let oRe  = out8[oI * 2    ];
+         let oIm  = out8[oI * 2 + 1];
+         let tRe  = FFT_FAC_8[j * 2 + 0];
+         let tIm  = FFT_FAC_8[j * 2 + 1];
+         let t_oRe = oRe * tRe - oIm * tIm;
+         let t_oIm = oRe * tIm + oIm * tRe;
+         out8[eI * 2    ] = eRe + t_oRe;
+         out8[eI * 2 + 1] = eIm + t_oIm;
+         out8[oI * 2    ] = eRe - t_oRe;
+         out8[oI * 2 + 1] = eIm - t_oIm;
+     }
+    } 
 
     return out8;
 } 
