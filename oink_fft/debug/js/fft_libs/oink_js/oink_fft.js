615000d38796afec83b9db381e14c3d049d7fb77
sch1z0net <eindudevommond@gmail.com>
2024-03-30 10:09:34 +0100

    Update oink_fft.js

--- oink_fft/debug/js/fft_libs/oink_js/oink_fft.js
+++ oink_fft/debug/js/fft_libs/oink_js/oink_fft.js
@@ -1,3 +1,4 @@
+import { fftReal32  } from './ffts/fft_32_unrolled.js'
 import { fftReal128  } from './ffts/fft_128_unrolled.js'
 import { fftReal256  } from './ffts/fft_256_unrolled.js'
 import { fftReal512  } from './ffts/fft_512_unrolled.js'
@@ -19,5 +20,6 @@ export {
     fftReal1024,
     fftReal512,
     fftReal256,
-    fftReal128
+    fftReal128,
+    fftReal32,
 };
