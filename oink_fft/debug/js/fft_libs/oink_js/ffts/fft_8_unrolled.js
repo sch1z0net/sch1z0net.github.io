bc049ebe14edeaca08418c70103dda2dea9eada4
sch1z0net <eindudevommond@gmail.com>
2024-04-05 10:53:36 +0200

    test

--- oink_fft/debug/js/fft_libs/oink_js/ffts/fft_8_unrolled.js
+++ oink_fft/debug/js/fft_libs/oink_js/ffts/fft_8_unrolled.js
@@ -66,6 +66,7 @@ function fftReal8(realInput) {
     // RADIX 2 (rolled) - FFT step for SIZE 8 
     ////////////////////////////////////////////////
     { 
+     /*
      for (let j = 0; j < 4; j++) { 
          let eI = 0 + j;
          let oI = 0 + j + 4;
@@ -82,6 +83,79 @@ function fftReal8(realInput) {
          out8[oI * 2    ] = eRe - t_oRe;
          out8[oI * 2 + 1] = eIm - t_oIm;
      }
+     */
+
+            let xA0re  = out8[0];
+            let xA0im  = 0;
+            let xA4re  = out8[8];
+            let xA4im  = 0;
+
+            let xA1re  = out8[2];
+            let xA1im  = out8[3];
+            let xA5re  = out8[10];
+            let xA5im  = out8[11];
+
+            let xA2re  = out8[4];
+            let xA2im  = 0;
+            let xA6re  = out8[12];
+            let xA6im  = 0;
+
+            let xA3re  =  xA1re;
+            let xA3im  = -xA1im;
+            let xA7re  =  xA5re;
+            let xA7im  = -xA5im;
+
+         
+
+         //FFT_FAC_8[0]
+         //FFT_FAC_8[1]
+         let resA0 = xA0re + xA4re;
+         let resA1 = 0;
+         let resA8 = xA0re - xA4re;
+         let resA9 = 0;
+
+         out8[0] =  resA0;
+         out8[1] =  resA1;
+         out8[8] =  resA8;
+         out8[9] =  resA9;
+
+         //FFT_FAC_8[2]
+         //FFT_FAC_8[3]
+         let tRe = 0.7071067690849304;  //FFT_FAC_8[2];
+         let resA2  = xA1re + (xA5re * tRe - xA5im *-tRe);
+         let resA3  = xA1im + (xA5re *-tRe + xA5im * tRe);
+         let resA10 = xA1re - (xA5re * tRe - xA5im *-tRe);
+         let resA11 = xA1im - (xA5re *-tRe + xA5im * tRe);
+
+         out8[2]  = resA2;
+         out8[3]  = resA3;
+         out8[10] = resA10;
+         out8[11] = resA11;
+
+         //FFT_FAC_8[4]
+         //FFT_FAC_8[5]
+         let resA4  =   xA2re;
+         let resA5  = - xA6re;
+         let resA12 =   xA2re;
+         let resA13 =   xA6re;
+
+         out8[4]  = resA4;
+         out8[5]  = resA5;
+         out8[12] = resA12;
+         out8[13] = resA13;
+
+         //FFT_FAC_8[6]
+         //FFT_FAC_8[7]
+         let resA6  =  xA1re + (xA5re * -tRe - xA5im * tRe);
+         let resA7  = -xA1im + (xA5re * -tRe + xA5im * tRe);
+         let resA14 =  xA1re - (xA5re * -tRe - xA5im * tRe);
+         let resA15 = -xA1im - (xA5re * -tRe + xA5im * tRe);
+
+         out8[6]  = resA6;
+         out8[7]  = resA7;
+         out8[14] = resA14;
+         out8[15] = resA15;
+
     } 
 
     return out8;
